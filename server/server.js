60ba429ab522429e4500b27435641939e1886db3
Tanveer058 <zardaritanveer058@gmail.com>
2025-08-26 12:27:09 +0500

    chnages in server.js

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(cors({
   // origin: 'http://localhost:3000',
   origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
 
